970c6aa581269f4bfd33b1ace06a9f0ae07a8f8d
seangnewman <seangnewman@msn.com>
2021-08-13 08:10:39 -0700

    Add project files.

---
+++ PizzaPlace/Shared/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PizzaPlace.Shared
+{
+    public class Pizza
+    {
+
+        public Pizza(int id, string name, decimal price, Spiciness spiciness)
+        {
+            Id = id;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Price = price;
+            Spiciness = spiciness;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public Spiciness Spiciness { get; private set; }
+    }
+}
---
+++ PizzaPlace/Shared/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace PizzaPlace.Shared
+{
+    public class State
+    {
+        public Menu Menu { get; set; } = new Menu();
+        public Basket Basket { get; set; } = new Basket();
+        public UI UI { get; set; } = new UI();
+
+        public decimal TotalPrice  => Basket.Orders.Sum(id => Menu.GetPizza(id).Price);
+    }
+}

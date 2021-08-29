c1db4b9c768df913a0f74132ba8c96480fe4297b
Sean Newman <seangnewman@msn.com>
2021-08-29 06:09:34 -0700

    Added Alert Messages

---
+++ OReilly_MicrosoftBlazor/Client/Pages/Index.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OReilly_MicrosoftBlazor.Client.Pages
+{
+    public partial class Index
+    {
+        public bool ShowAlert { get; set; } = true;
+        public void ToggleAlert()
+        => ShowAlert = !ShowAlert;
+    }
+}
